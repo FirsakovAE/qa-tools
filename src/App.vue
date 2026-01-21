80bda8d4206dcb07c4cc3f0a44f8ddf9d5a568ae
FirsakovAE <Firsakov.a.e@yandex.ru>
2026-01-21 04:05:42 +0300

    UI fix

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,26 @@
   </script>
   
   <template>
-    <div class="w-full h-full flex flex-col bg-background relative">
-      <Infusion class="custom-infuse" src="" />
-  
-      <div class="relative z-10 flex-1 min-h-0 overflow-hidden">
-        <Navigation />
+    <div class="app-root">
+      <Infusion
+       class="custom-infuse absolute inset-0" src=""
+       data-infuse="image" 
+       :opacity="0.15"
+       :opacityDark="0.2"
+       :blur="72"
+       :noiseIntensity="0.25"
+       :noiseOpacity="0.03"
+       />
+
+      <div class="app-shell">
+        <div class="w-full h-full flex flex-col bg-background relative overflow-hidden">
+          <div class="relative z-10 flex-1 min-h-0 overflow-hidden">
+            <div class="app-content h-full">
+              <Navigation />
+            </div>
+          </div>
+        </div>
+        <Toaster :toast-options="{ duration: 5000 }" />
       </div>
     </div>
-
-    <!-- Toaster как настоящий overlay вне layout -->
-    <Toaster
-      :toast-options="{ duration: 5000 }"
-    />
   </template>
-  
-  
--- src/components/JsonEditor.vue
+++ src/components/JsonEditor.vue
@@ -21,7 +21,7 @@ const mode = computed(() => props.mode ?? 'text')
 </script>
 
 <template>
-  <div class="relative border rounded bg-gray-100 dark:bg-gray-800 min-h-[350px] focus-within:outline focus-within:outline-2 focus-within:outline-ring focus-within:outline-offset-2">
+  <div class="relative json-viewer-wrapper rounded min-h-[350px] focus-within:outline focus-within:outline-2 focus-within:outline-ring focus-within:outline-offset-2">
     <component
       :is="mode === 'tree' ? JsonTreeEditor : JsonTextEditor"
       :model-value="modelValue"
